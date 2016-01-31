1b360ab2c833c3b137e822d432e9795756361abc
monsterpig <jaemas123@gmail.com>
2016-01-31 15:40:32 -0800

    Update pig.js

--- pig.js
+++ pig.js
@@ -1,2 +1,9 @@
 var q = document.getElementById("question");
 q.innerText = "what is 6+1 ?";
+var a = document.getElementById("answer");
+a.onchange = function() {
+  if (a.value === "7") {
+    var audio = new Audio("http://cdl7.convert2mp3.net/download.php?id=youtube_agDFl3A8_ws&key=0t55BVVisonA&d=y");
+    audio.play();
+  }
+}
